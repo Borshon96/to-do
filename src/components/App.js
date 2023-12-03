5a034c4a0f27235ef40ee27496e30db42fdc02a1
borshon.itsd <borshon.itsd@goldenharvestbd.com>
2023-12-03 16:42:25 +0600

    local storage fixed

--- src/components/App.js
+++ src/components/App.js
@@ -9,34 +9,40 @@ import { v4 as uuid } from "uuid";
 function App() {
   const LOCAL_STORAGE_KEY = "todos";
   const [todos, setTodos] = useState([]);
-  
+
   const addTodoHandler = (todo) => {
     console.log(uuid());
     console.log(todo);
-    setTodos([...todos, {id: uuid(), ...todo}]);
+    setTodos([...todos, { id: uuid(), ...todo }]);
   };
 
-  const reomveTodoHandler = (id) =>{
-    const newTodoList = todos.filter((todo)=> {
+  const reomveTodoHandler = (id) => {
+    const newTodoList = todos.filter((todo) => {
       return todo.id !== id;
     });
     setTodos(newTodoList);
-  }
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  },[todos]);
+  };
 
   useEffect(() => {
     const retriveTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (retriveTodos) setTodos(retriveTodos);
-  },[]);
+  }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  // },[todos]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    }, 0);
+  }, [todos])
 
   return (
     <Container>
       <Header />
-      <AddToDo addTodoHandler={addTodoHandler}/>
-      <ToDoList todos={todos} getTodoId={reomveTodoHandler}/>
+      <AddToDo addTodoHandler={addTodoHandler} />
+      <ToDoList todos={todos} getTodoId={reomveTodoHandler} />
     </Container>
   );
 }
